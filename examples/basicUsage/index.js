7c947026021429e75a6a81bcb2fb4a3c309c5f7a
@micahstubbs <micah.stubbs@gmail.com>
2018-09-05 13:09:08 -0700

    basicUsage

---
+++ examples/basicUsage/index.js
@@ -0,0 +1,28 @@
+/* global vis */
+
+// create an array with nodes
+var nodes = new vis.DataSet([
+  { id: 1, label: 'Node 1' },
+  { id: 2, label: 'Node 2' },
+  { id: 3, label: 'Node 3' },
+  { id: 4, label: 'Node 4' },
+  { id: 5, label: 'Node 5' }
+])
+
+// create an array with edges
+var edges = new vis.DataSet([
+  { from: 1, to: 3 },
+  { from: 1, to: 2 },
+  { from: 2, to: 4 },
+  { from: 2, to: 5 },
+  { from: 3, to: 3 }
+])
+
+// create a network
+var container = document.getElementById('mynetwork')
+var data = {
+  nodes: nodes,
+  edges: edges
+}
+var options = {}
+new vis.Network(container, data, options)
