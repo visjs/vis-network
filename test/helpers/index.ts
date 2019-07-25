8ea5e64c22e1420844dc738b80b42bf41cedfe9c
Tomáš Vyčítal <tom.vycital@gmail.com>
2019-07-25 10:44:41 +0200

    add and improve test coverage (#8)
    * Update vis-data and commit package-lock
    
    * Prepare the package for TypeScript
    
    * Update dependencies
    
    * Fix coverage reports
    
    * Fix Mocha never finishing
    
    * New package lock (who knows why)
    
    * Regenerate package lock
    
    If anybody has any advice about merging package locks I'm all ears.
    
    * Add coverage to root index.html
    
    * Set up ESLint with Prettier for TS files
    
    Prettier is not used for JavaScript files at the moment.
    
    * Add tsconfig
    
    * Install missing deps
    
    * Fix ESLint config
    
    * Add NodesHandler test
    
    Brings the coverage from red/yellow to yellow/green numbers.

---
+++ test/helpers/index.ts
@@ -0,0 +1,53 @@
+/**
+ * Prevents supplied object from being modified.
+ *
+ * @remarks
+ * Has no problem with cycles.
+ * Can receive frozen or parially frozen objects.
+ *
+ * @typeParam T - The type of the object.
+ *
+ * @param object - The object to be recursively frozen.
+ * @param freeze - Function that does the freezing. Object.seal or anything with the same API can be used instead.
+ *
+ * @returns The frozen object (the same instance as object param).
+ */
+export function deepFreeze<T extends object>(
+  object: T,
+  freeze: (object: any) => any = Object.freeze.bind(Object)
+): T {
+  const alreadyFrozen = new Set<any>()
+
+  /**
+   * Recursivelly freezes objects using alreadyFrozen to prevent infinite cycles.
+   *
+   * @param object - The object to be recursively frozen.
+   *
+   * @returns The frozen object (the same instance as object param).
+   */
+  function recursivelyFreeze(object: any): any {
+    // Prevent double freezing (could lead to stack overflow due to an infinite cycle)
+    // Object.isFrozen is not used here because frozen objects can have unfrozen objects in their properties.
+    if (alreadyFrozen.has(object)) {
+      return object
+    }
+    alreadyFrozen.add(object)
+
+    // Retrieve the property names defined on object
+    const names = Object.getOwnPropertyNames(object)
+
+    // Freeze properties before freezing the object
+    for (const name of names) {
+      const prop = object[name]
+      if (prop && typeof prop === 'object') {
+        recursivelyFreeze(prop)
+      } else {
+        freeze(prop)
+      }
+    }
+
+    return freeze(object)
+  }
+
+  return recursivelyFreeze(object)
+}
