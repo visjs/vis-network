95877ff855b8fad0317576056a287dd0cd7f3bba
Tomáš Vyčítal <tom.vycital@gmail.com>
2019-07-27 13:01:09 +0200

    Maintain old code style

--- rollup.config.js
+++ rollup.config.js
@@ -1,69 +1,69 @@
-import commonjs from 'rollup-plugin-commonjs'
-import resolve from 'rollup-plugin-node-resolve'
-import babel from 'rollup-plugin-babel'
-import typescript from 'rollup-plugin-typescript2'
-import banner from 'rollup-plugin-banner'
-import minify from 'rollup-plugin-babel-minify'
-import genHeader from './lib/header'
+import commonjs from 'rollup-plugin-commonjs';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import typescript from 'rollup-plugin-typescript2';
+import minify from 'rollup-plugin-babel-minify';
+import banner from 'rollup-plugin-banner';
+import genHeader from './lib/header';
 
 // TypeScript because Babel transpiles modules in isolation, therefore no type reexports.
 // CommonJS because Babel is not 100 % ESM.
 
 const plugins = [
-  resolve({
-    extensions: ['.ts', '.js', '.json'],
-  }),
-  typescript({
-    tsconfig: 'tsconfig.code.json',
-  }),
-  commonjs(),
-  babel({
-    extensions: ['.ts', '.js'],
-    runtimeHelpers: true,
-  }),
-  banner(genHeader('network')),
+	nodeResolve({
+		extensions: ['.ts', '.js', '.json']
+	}),
+	typescript({
+		tsconfig: 'tsconfig.code.json'
+	}),
+	commonjs(),
+	babel({
+		extensions: ['.ts', '.js'],
+		runtimeHelpers: true
+	}),
+	banner(genHeader('network'))
 ]
 const minPlugins = [
-  ...plugins.slice(0, 4),
-  minify({ comments: false }),
-  ...plugins.slice(4)
+	...plugins.slice(0, 4),
+	minify({ comments: false }),
+	...plugins.slice(4)
 ]
 
 export default [
-  {
-    input: 'lib/index.ts',
-    output: {
-      file: 'dist/esm.js',
-      format: 'esm',
-    },
-    plugins,
-  },
-  {
-    input: 'lib/index.ts',
-    output: {
-      file: 'dist/umd.js',
-      format: 'umd',
-      exports: 'named',
-      name: 'vis',
-    },
-    plugins,
-  },
-  {
-    input: 'lib/index.ts',
-    output: {
-      file: 'dist/esm.min.js',
-      format: 'esm',
-    },
-    plugins: minPlugins,
-  },
-  {
-    input: 'lib/index.ts',
-    output: {
-      file: 'dist/umd.min.js',
-      format: 'umd',
-      exports: 'named',
-      name: 'vis',
-    },
-    plugins: minPlugins,
-  },
+	{
+		input: 'lib/index.ts',
+		output: {
+			file: 'dist/esm.js',
+			format: 'esm'
+		},
+		plugins
+	},
+	{
+		input: 'lib/index.ts',
+		output: {
+			file: 'dist/umd.js',
+			format: 'umd',
+			exports: 'named',
+			name: 'vis'
+		},
+		plugins
+	},
+	{
+		input: 'lib/index.ts',
+		output: {
+			file: 'dist/esm.min.js',
+			format: 'esm'
+		},
+		plugins: minPlugins
+	},
+	{
+		input: 'lib/index.ts',
+		output: {
+			file: 'dist/umd.min.js',
+			format: 'umd',
+			exports: 'named',
+			name: 'vis'
+		},
+		plugins: minPlugins,
+	}
 ]
