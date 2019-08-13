83b3d841c08a5aca64c4704124a942637f9ba56f
Tomáš Vyčítal <tom.vycital@gmail.com>
2019-08-13 19:29:34 +0200

    chore(dist): update

--- dist/vis-network.esm.js
+++ dist/vis-network.esm.js
@@ -5,7 +5,7 @@
  * A dynamic, browser-based visualization library.
  *
  * @version 0.0.0-no-version
- * @date    2019-08-13T16:59:32Z
+ * @date    2019-08-13T17:27:41Z
  *
  * @copyright (c) 2011-2017 Almende B.V, http://almende.com
  * @copyright (c) 2018-2019 visjs contributors, https://github.com/visjs
