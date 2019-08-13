eff401381e45588fbb8700d00f3ce6f3a9846037
Tomáš Vyčítal <tom.vycital@gmail.com>
2019-08-13 19:03:10 +0200

    chore(build): bundle CSS using Rollup

--- rollup.config.js
+++ rollup.config.js
@@ -4,71 +4,79 @@ import babel from 'rollup-plugin-babel';
 import typescript from 'rollup-plugin-typescript2';
 import minify from 'rollup-plugin-babel-minify';
 import genHeader from './lib/header';
+import css from 'rollup-plugin-css-porter';
 
 // TypeScript because Babel transpiles modules in isolation, therefore no type reexports.
 // CommonJS because Babel is not 100 % ESM.
 
-const plugins = [
-	nodeResolve({
+const plugins = {
+	nodeResolve: nodeResolve({
 		extensions: ['.ts', '.js', '.json']
 	}),
-	typescript({
+	typescript: typescript({
 		tsconfig: 'tsconfig.code.json'
 	}),
-	commonjs(),
-	babel({
+	commonjs: commonjs(),
+	babel: babel({
 		extensions: ['.ts', '.js'],
 		runtimeHelpers: true
+	}),
+	minify: minify({ comments: false }),
+	cssRaw: css({
+		raw: 'dist/vis-network.css',
+		minified: false
+	}),
+	cssMin: css({
+		raw: false,
+		minified: 'dist/vis-network.min.css'
 	})
-]
-const minPlugins = [
-	...plugins.slice(),
-	minify({ comments: false })
-]
+}
 
 export default [
 	{
 		input: 'lib/index-bundle.ts',
-		output: {
+		output: [{
 			file: 'dist/vis-network.esm.js',
 			format: 'esm',
 			banner: genHeader('network'),
 			sourcemap: true
-		},
-		plugins
-	},
-	{
-		input: 'lib/index-bundle.ts',
-		output: {
+		}, {
 			file: 'dist/vis-network.js',
 			format: 'umd',
 			exports: 'named',
 			name: 'vis',
 			banner: genHeader('network'),
 			sourcemap: true
-		},
-		plugins
+		}],
+		plugins: [
+			plugins.commonjs,
+			plugins.nodeResolve,
+			plugins.cssRaw,
+			plugins.typescript,
+			plugins.babel
+		]
 	},
 	{
 		input: 'lib/index-bundle.ts',
-		output: {
+		output: [{
 			file: 'dist/vis-network.esm.min.js',
 			format: 'esm',
 			banner: genHeader('network'),
 			sourcemap: true
-		},
-		plugins: minPlugins
-	},
-	{
-		input: 'lib/index-bundle.ts',
-		output: {
+		}, {
 			file: 'dist/vis-network.min.js',
 			format: 'umd',
 			exports: 'named',
 			name: 'vis',
 			banner: genHeader('network'),
 			sourcemap: true
-		},
-		plugins: minPlugins,
+		}],
+		plugins: [
+			plugins.commonjs,
+			plugins.nodeResolve,
+			plugins.cssMin,
+			plugins.typescript,
+			plugins.babel
+		]
 	}
 ]
