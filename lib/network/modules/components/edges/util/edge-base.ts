159b1f93f9c592eb909df1977b8b164ea0404472
Piratuks <evaldas.laureckas@gmail.com>
2020-01-31 08:38:21 +0200

    no message

--- lib/network/modules/components/edges/util/edge-base.ts
+++ lib/network/modules/components/edges/util/edge-base.ts
@@ -534,8 +534,8 @@ export abstract class EdgeBase<Via = undefined> implements EdgeType {
     if (!this.options.selfReference.renderBehindTheNode) {
       //render only parts which are not overlaping with parent node
       //need to find x,y of from point and x,y to point
-      //calculating radiangs
-      let low = this.options.selfReference.angle - 0.1;
+      //calculating radians
+      let low = this.options.selfReference.angle;
       let high = this.options.selfReference.angle + Math.PI;
       const pointTFrom = this._findBorderPositionCircle(this.from, ctx, {
         x,
@@ -544,8 +544,6 @@ export abstract class EdgeBase<Via = undefined> implements EdgeType {
         high,
         direction: -1
       });
-      low = this.options.selfReference.angle + 0.1;
-      high = this.options.selfReference.angle + Math.PI;
       const pointTTo = this._findBorderPositionCircle(this.from, ctx, {
         x,
         y,
@@ -776,7 +774,7 @@ export abstract class EdgeBase<Via = undefined> implements EdgeType {
       const [x, y, radius] = this._getCircleData(ctx);
 
       if (position === "from") {
-        const low = this.options.selfReference.angle - 0.1;
+        const low = this.options.selfReference.angle;
         const high = this.options.selfReference.angle + Math.PI;
 
         const pointT = this._findBorderPositionCircle(this.from, ctx, {
@@ -789,7 +787,7 @@ export abstract class EdgeBase<Via = undefined> implements EdgeType {
         angle = pointT.t * -2 * Math.PI + 1.5 * Math.PI + 0.1 * Math.PI;
         arrowPoint = pointT;
       } else if (position === "to") {
-        const low = this.options.selfReference.angle + 0.1;
+        const low = this.options.selfReference.angle;
         const high = this.options.selfReference.angle + Math.PI;
 
         const pointT = this._findBorderPositionCircle(this.from, ctx, {
