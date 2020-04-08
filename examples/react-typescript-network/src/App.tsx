a46bdd11dbce407a44860efc71b4971b6d89aee2
Brijen Shah <brshah@microsoft.com>
2020-04-07 17:37:43 -0700

    Updated formatting

--- examples/react-typescript-network/src/App.tsx
+++ examples/react-typescript-network/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router';
-import Layout  from './components/Layout';
+import Layout from './components/Layout';
 import { BasicGraph } from './components/BasicGraph';
 
 export default function App() {
--- examples/react-typescript-network/src/components/BasicGraph.tsx
+++ examples/react-typescript-network/src/components/BasicGraph.tsx
@@ -1,22 +1,22 @@
 import React, { useEffect } from 'react';
 import { Network, DataSet, Node, Edge, Data, Options } from "vis-network";
 
-interface BasicGraphProps {}
+interface BasicGraphProps { }
 
-const nodes : DataSet<Node> = [
+const nodes: DataSet<Node> = [
     { id: 1, label: "Node 1" },
-  { id: 2, label: "Node 2" },
-  { id: 3, label: "Node 3" },
-  { id: 4, label: "Node 4" },
-  { id: 5, label: "Node 5" }
+    { id: 2, label: "Node 2" },
+    { id: 3, label: "Node 3" },
+    { id: 4, label: "Node 4" },
+    { id: 5, label: "Node 5" }
 ];
 
-const edges : DataSet<Edge> = [
+const edges: DataSet<Edge> = [
     { from: 1, to: 3 },
-  { from: 1, to: 2 },
-  { from: 2, to: 4 },
-  { from: 2, to: 5 },
-  { from: 3, to: 3 }
+    { from: 1, to: 2 },
+    { from: 2, to: 4 },
+    { from: 2, to: 5 },
+    { from: 3, to: 3 }
 ];
 
 const data: Data = {
@@ -26,7 +26,7 @@ const data: Data = {
 
 const options: Options = {}
 
-export const BasicGraph : React.FC<BasicGraphProps> = (props: BasicGraphProps) => {
+export const BasicGraph: React.FC<BasicGraphProps> = (props: BasicGraphProps) => {
 
     useEffect(() => {
         const container: HTMLElement = document.getElementById("graphId") as HTMLElement;
@@ -34,6 +34,6 @@ export const BasicGraph : React.FC<BasicGraphProps> = (props: BasicGraphProps) =
     });
 
     return (
-        <div id="graphId" style={{height: '100%', width: '100%'}}></div>
+        <div id="graphId" style={{ height: '100%', width: '100%' }}></div>
     )
 }
