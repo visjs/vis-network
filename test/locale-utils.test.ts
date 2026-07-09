3ea0c177bd4a161f5758db36a94a8dfec457deec
Tomina <tomina@vycital.eu>
2026-07-09 17:39:21 +0200

    test(locale-utils): migrate from Sazerac to Chai
    Sazerac haven't seen an update in 6 years and we only used it for this
    single test. It's not even particularly complicated to achieve the same
    with plain Chai so we may as well remove the dependency.

--- test/locale-utils.test.ts
+++ test/locale-utils.test.ts
@@ -1,4 +1,4 @@
-import { test, given } from "sazerac";
+import { expect } from "chai";
 import { deepFreeze } from "./helpers/index.ts";
 
 import { normalizeLanguageCode } from "../lib/network/locale-utils.ts";
@@ -12,34 +12,38 @@ const someLocales = deepFreeze({
 });
 
 describe("Locale utils", function () {
-  test(normalizeLanguageCode, (): void => {
-    given(noLocales, "en").expect("en");
-    given(noLocales, "en-US").expect("en");
-    given(noLocales, "en_US").expect("en");
-    given(noLocales, "sk").expect("en");
-    given(noLocales, "sk-SK").expect("en");
-    given(noLocales, "sk_SK").expect("en");
-    given(someLocales, " cs").expect("en");
-    given(someLocales, "-CZ").expect("en");
-    given(someLocales, "CS-CZ").expect("cs-CZ");
-    given(someLocales, "Cs_I-am_an alligator!").expect("cs");
-    given(someLocales, "Cs_cZ").expect("cs-CZ");
-    given(someLocales, "I-am_an alligator!").expect("en");
-    given(someLocales, "cs SK").expect("cs-SK");
-    given(someLocales, "cs").expect("cs");
-    given(someLocales, "cs").expect("cs");
-    given(someLocales, "cs-SK").expect("cs-SK");
-    given(someLocales, "cs-US").expect("cs");
-    given(someLocales, "cs-cz").expect("cs-CZ");
-    given(someLocales, "cs/SK").expect("cs-SK");
-    given(someLocales, "cs_SK").expect("cs-SK");
-    given(someLocales, "cs_US").expect("cs");
-    given(someLocales, "cs_cz").expect("cs-CZ");
-    given(someLocales, "en").expect("en");
-    given(someLocales, "en-US").expect("en");
-    given(someLocales, "en_US").expect("en");
-    given(someLocales, "sk").expect("en");
-    given(someLocales, "sk-SK").expect("en");
-    given(someLocales, "sk_SK").expect("en");
+  it("normalizeLanguageCode", (): void => {
+    expect(normalizeLanguageCode(noLocales, "en")).to.equal("en");
+    expect(normalizeLanguageCode(noLocales, "en-US")).to.equal("en");
+    expect(normalizeLanguageCode(noLocales, "en_US")).to.equal("en");
+    expect(normalizeLanguageCode(noLocales, "sk")).to.equal("en");
+    expect(normalizeLanguageCode(noLocales, "sk-SK")).to.equal("en");
+    expect(normalizeLanguageCode(noLocales, "sk_SK")).to.equal("en");
+    expect(normalizeLanguageCode(someLocales, " cs")).to.equal("en");
+    expect(normalizeLanguageCode(someLocales, "-CZ")).to.equal("en");
+    expect(normalizeLanguageCode(someLocales, "CS-CZ")).to.equal("cs-CZ");
+    expect(
+      normalizeLanguageCode(someLocales, "Cs_I-am_an alligator!"),
+    ).to.equal("cs");
+    expect(normalizeLanguageCode(someLocales, "Cs_cZ")).to.equal("cs-CZ");
+    expect(normalizeLanguageCode(someLocales, "I-am_an alligator!")).to.equal(
+      "en",
+    );
+    expect(normalizeLanguageCode(someLocales, "cs SK")).to.equal("cs-SK");
+    expect(normalizeLanguageCode(someLocales, "cs")).to.equal("cs");
+    expect(normalizeLanguageCode(someLocales, "cs")).to.equal("cs");
+    expect(normalizeLanguageCode(someLocales, "cs-SK")).to.equal("cs-SK");
+    expect(normalizeLanguageCode(someLocales, "cs-US")).to.equal("cs");
+    expect(normalizeLanguageCode(someLocales, "cs-cz")).to.equal("cs-CZ");
+    expect(normalizeLanguageCode(someLocales, "cs/SK")).to.equal("cs-SK");
+    expect(normalizeLanguageCode(someLocales, "cs_SK")).to.equal("cs-SK");
+    expect(normalizeLanguageCode(someLocales, "cs_US")).to.equal("cs");
+    expect(normalizeLanguageCode(someLocales, "cs_cz")).to.equal("cs-CZ");
+    expect(normalizeLanguageCode(someLocales, "en")).to.equal("en");
+    expect(normalizeLanguageCode(someLocales, "en-US")).to.equal("en");
+    expect(normalizeLanguageCode(someLocales, "en_US")).to.equal("en");
+    expect(normalizeLanguageCode(someLocales, "sk")).to.equal("en");
+    expect(normalizeLanguageCode(someLocales, "sk-SK")).to.equal("en");
+    expect(normalizeLanguageCode(someLocales, "sk_SK")).to.equal("en");
   });
 });
